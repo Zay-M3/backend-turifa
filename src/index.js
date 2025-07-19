390d3643f2efae063133b3c20a7a4a6dadcf50e1
David <oscardavidestradabetancourth@gmail.com>
2025-07-19 09:49:50 -0500

    logs?

--- src/index.js
+++ src/index.js
@@ -7,8 +7,4 @@ app.get("/", (req, res) => {
   res.send("Server is runing");
 });
 
-const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+export default app;
